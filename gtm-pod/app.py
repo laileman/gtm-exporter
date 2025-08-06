4b7ed37f912c511bfbd0f9de0c6f026a5bc29db4
Leman <leman.lai@firstfun.com>
2025-08-06 11:53:27 +0800

    gtm pod: add loglevel

--- gtm-pod/app.py
+++ gtm-pod/app.py
@@ -14,4 +14,5 @@ def read_health():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=config["host"], port=config["port"])
+    print(f"server start on {config['host']}:{config['port']}")
+    uvicorn.run(app, host=config["host"], port=config["port"], log_level=config["log_level"])
